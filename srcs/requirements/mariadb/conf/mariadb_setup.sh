dbda331d24f6bb722ce92eb92b0c0ddc41ca4b94
lvirgini <lvirgini.42.fr>
2022-02-21 23:30:00 +0100

    wp/mariadb link OK, wp-config a faire avec les variables d'env

--- srcs/requirements/mariadb/conf/mariadb_setup.sh
+++ srcs/requirements/mariadb/conf/mariadb_setup.sh
@@ -6,19 +6,28 @@
 
 #lanch mysql and sleep for lets time to mysql start
 /usr/bin/mysqld -u root & sleep 2 ###
-
-mysql -u root -e "DROP DATABASE test;" ####
+# usr/bin/mysqld_safe --datadir=/var/lib/mysql
 
 # create if no exist database and user with password in .env
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
-    CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';\
-    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; \
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
+#     CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';\
+#     GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; \
+#     FLUSH PRIVILEGES;"
+
+ mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
+ CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';\
+    GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%'; \
     FLUSH PRIVILEGES;"
 
-# change root password
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
-# mysql -u root -e "FLUSH PRIVILEGES;" ###
+# # change root password
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
+# # mysql -u root -e "FLUSH PRIVILEGES;" ###
+mysql -u root -e "SET PASSWORD FOR 'root'@'%' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
 
 # rc-service mysql restart in alpine
 pkill mysqld
-/usr/bin/mysqld -u root
+pkill mariadb
+
+# /usr/bin/mysqld -u root 
+/usr/bin/mysqld -u root --skip-name-resolve --datadir=/var/lib/mysql --bind_address=0.0.0.0 --port=3306 --skip-networking=off --socket=/var/run/mysqld/mysqld.sock
+# usr/bin/mysqld_safe -u root --skip-name-resolve --datadir=/var/lib/mysql --bind_address=0.0.0.0 --port=3306 --skip-networking=off --socket=/var/run/mysqld/mysqld.sock
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -4,10 +4,10 @@
  *
  * Ce fichier est utilisé par le script de création de wp-config.php pendant
  * le processus d’installation. Vous n’avez pas à utiliser le site web, vous
- * pouvez simplement renommer ce fichier en « wp-config.php » et remplir les
+ * pouvez simplement renommer ce fichier en « wp-config.php » et remplir les
  * valeurs.
  *
- * Ce fichier contient les réglages de configuration suivants :
+ * Ce fichier contient les réglages de configuration suivants :
  *
  * Réglages MySQL
  * Préfixe de table
@@ -20,29 +20,21 @@
  * @package WordPress
  */
 
-
-define( 'DB_COLLATE', '' );
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME',  getenv('MYSQL_DATABASE') );
+define( 'DB_NAME', 'wordpress_db' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', getenv('MYSQL_USER') );
+define( 'DB_USER', 'user42' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', getenv('MYSQL_USER_PASSWORD') );
+define( 'DB_PASSWORD', 'lvirgini42' );
 
 /** Adresse de l’hébergement MySQL. */
-// define( 'DB_HOST', 'mariadb' );
-// define( 'DB_HOST', '0.0.0.0:mariadb' );
-// define( 'DB_HOST', '127.0.0.1:3306' );
-// define( 'DB_HOST', 'localhost:3306' );
-// define( 'DB_HOST', 'localhost' );
-// define( 'DB_HOST', '0.0.0.0' );
-define( 'DB_HOST', '0.0.0.0:3306' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /**
  * Type de collation de la base de données.
@@ -61,16 +53,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-
-define('AUTH_KEY',         'uMH7YY3h8J{J`FdtFu P-<V a)*xw![)?I@_6}XUgEf?{c`;]q8:.S+&=T@zDKA$');
-define('SECURE_AUTH_KEY',  'myhK+-JJVC|8GOQ}Rc-4d|++kMc_qNoY99:I!ZJpx?LX)j+dpA1P}*O9c(ufGi)E');
-define('LOGGED_IN_KEY',    'RH76^pfsr:`EZ YdEsc)$tUx)ArC$8KXVPR!V2m;}VNCB9aWh|/e>:BN^ ~Y%2+i');
-define('NONCE_KEY',        'XjSr,EmjC(|)56|:*h~E!ze3G<E4q#~ftGK>x*v=dbK<M=[17]#&d{{Q4<|%7o G');
-define('AUTH_SALT',        '@8M,zr.U9_X`~6Lo@6gKod3hjQ8@G/MWej:v6+oOL]m0Hg`|u@%nEg;/-3MmL_jF');
-define('SECURE_AUTH_SALT', 'sfD4?+|n;G))TF&3js-V]6( v`<,4$l|g,Iz_Ge}.jfGm*F+m6c#v^N5RAby/tg:');
-define('LOGGED_IN_SALT',   'La F#<Z>+g^_Ven;pJ:GZ quh7D1PB-ya@E aE^9q|IzS-/;K-?=8f*v&DPyIQ z');
-define('NONCE_SALT',       'vSW?WBI_!uB+3X-!*y-Zp~|)G>HqFvZka $;zn;w H4zw)8oEuD+NP@4Lut*+hFJ');
-
+define( 'AUTH_KEY',         'nKRi`~|oHF`=Q+M:t+n2]1`<z]m~GYeXdl,;_JRLtzE/`Q5F`%h0L#h=;9YAM5,k' );
+define( 'SECURE_AUTH_KEY',  'I?v0y*iO02Ud*/pZ{7 J.s5e?(/!yT?.{CQ|^;Z23 K7iB<g,H-FO):JLvm9Kv`M' );
+define( 'LOGGED_IN_KEY',    'KKFU4>3G$?M0#jiUJ+Ox~#~S=/#dKaz(i,t^Hs8f1<M,!tv?Y6:eGc}129bx~0Q{' );
+define( 'NONCE_KEY',        '?qKDPyK,81o|PLX~(<5 RTk>E8vtqm/OBdZky+i,x[}(0q^<~VK,Bghks`?<>S^1' );
+define( 'AUTH_SALT',        '%l4#`jUZ@0 H>23q~qPkO}Daks#N^yHbBcy5&YHhsW#B0l:M:}au-Cj}FuTINMl&' );
+define( 'SECURE_AUTH_SALT', '%Eq 7iwP2.>3E<wO`pEWwf{J9_9j>?x$_S/tJ3s~=Ge32.<-mrM0c:@WzVq]^J|y' );
+define( 'LOGGED_IN_SALT',   'hVJl-k+Y-u[X6-8b`+vw>E-WySvk6;{3IO-(Qalw8<q4I2cr(u.y/O64!7#t/`}y' );
+define( 'NONCE_SALT',       'xn2.b`5[o{|dE$2/N,I:[Fh){n0qeb.j]MWPA;co`(sn8z9R~8xEZ[<SS>|qgc@F' );
 /**#@-*/
 
 /**
@@ -78,7 +68,7 @@ define('NONCE_SALT',       'vSW?WBI_!uB+3X-!*y-Zp~|)G>HqFvZka $;zn;w H4zw)8oEuD+
  *
  * Vous pouvez installer plusieurs WordPress sur une seule base de données
  * si vous leur donnez chacune un préfixe unique.
- * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
+ * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
  */
 $table_prefix = 'wp_';
 
